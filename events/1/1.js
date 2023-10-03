3eac1121d2a256e88f4ad28fc7026603e757422d
truffie <somewertruk@gmail.com>
2023-10-03 22:29:53 +0300

    first task

---
+++ events/1/1.js
@@ -0,0 +1,5 @@
+// Необходимо отобразить кнопку с надписью «Нажми на меня». По клику на нее
+// вывести alert с сообщением
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => alert('Спасибо что нажали на меня!'))
