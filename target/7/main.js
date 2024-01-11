ba25f56ccbe9433c2e96c9959c00437838bf0ee9
Truffie <trufanov.vl2013@gmail.com>
2024-01-11 11:38:55 +0300

    feat: add 7 task

--- target/7/main.js
+++ target/7/main.js
@@ -1,32 +1,32 @@
-let choose = document.querySelector('.choose')
-let cont = document.querySelector('.container');
-let p = document.querySelectorAll('p');
-let flag = false;
+// let choose = document.querySelector('.choose')
+// let cont = document.querySelector('.container');
+// let p = document.querySelectorAll('p');
+// let flag = false;
 
-p.forEach((el) => el.addEventListener(('mouseover'), () => el.style.backgroundColor = '#f2f2f2'))
+// p.forEach((el) => el.addEventListener(('mouseover'), () => el.style.backgroundColor = '#f2f2f2'))
 
-p.forEach((el) => el.addEventListener(('mouseleave'), () => el.style.backgroundColor = '#fff'))
+// p.forEach((el) => el.addEventListener(('mouseleave'), () => el.style.backgroundColor = '#fff'))
 
-choose.addEventListener(('click'), () => {
-  p.forEach((el) => {
+// choose.addEventListener(('click'), () => {
+//   p.forEach((el) => {
 
-    if (el.textContent === choose.textContent) {
-      el.style.color = "red"
-    } else {
-      el.style.color = "black"
-    }
-  })
+//     if (el.textContent === choose.textContent) {
+//       el.style.color = "red"
+//     } else {
+//       el.style.color = "black"
+//     }
+//   })
 
-  if (!flag) {
-    cont.style.visibility = 'visible'
-    flag = true;
-  } else {
-    cont.style.visibility = 'hidden'
-    flag = false;
-  }
-})
-cont.addEventListener(('click'), (event) => {
-  choose.textContent = event.target.textContent
-  cont.style.visibility = 'hidden'
-  flag = false;
-})
+//   if (!flag) {
+//     cont.style.visibility = 'visible'
+//     flag = true;
+//   } else {
+//     cont.style.visibility = 'hidden'
+//     flag = false;
+//   }
+// })
+// cont.addEventListener(('click'), (event) => {
+//   choose.textContent = event.target.textContent
+//   cont.style.visibility = 'hidden'
+//   flag = false;
+// })
